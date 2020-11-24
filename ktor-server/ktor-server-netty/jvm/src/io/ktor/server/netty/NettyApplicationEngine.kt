c793b12ce0e88a1f911f3dc23bafd72200c4d5a8
rsinukov <rxsinukov@gmail.com>
2020-11-24 17:36:56 +0100

    KTOR-1358 Propagate parentCoroutineContext to handlers

--- ktor-server/ktor-server-netty/jvm/src/io/ktor/server/netty/NettyApplicationEngine.kt
+++ ktor-server/ktor-server-netty/jvm/src/io/ktor/server/netty/NettyApplicationEngine.kt
@@ -123,7 +123,9 @@ public class NettyApplicationEngine(
                 childHandler(
                     NettyChannelInitializer(
                         pipeline, environment,
-                        callEventGroup, engineDispatcherWithShutdown, dispatcherWithShutdown,
+                        callEventGroup,
+                        engineDispatcherWithShutdown,
+                        environment.parentCoroutineContext + dispatcherWithShutdown,
                         connector,
                         configuration.requestQueueLimit,
                         configuration.runningLimit,
